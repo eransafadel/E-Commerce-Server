8908e84ab623bdbb3baad5bf9a2610d42577f794
= <=>
2022-08-02 16:03:11 +0300

    fully impl product controller

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,11 +1,17 @@
 const router = require("express").Router();
 const {verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("../controllers/verifyToken");
 
-const {createProduct} = require("../controllers/product");
+const {createProduct,updateProduct,deleteProduct,getProduct,getAllProducts} = require("../controllers/product");
 
 
-//CREATE PRODUCT
-router.post("/", verifyTokenAndAdmin,createProduct );
+
+router.post("/", verifyTokenAndAdmin,createProduct );// CREATE PRODUCT
+router.put("/:id", verifyTokenAndAdmin,updateProduct );// UPDATE PRODUCT
+router.delete("/:id", verifyTokenAndAdmin,deleteProduct );// DELETE PRODUCT
+router.get("/find/:id",getProduct );// GET PRODUCT
+router.get("/",getAllProducts );// GET ALL PRODUCTS
+
+
 
  
 
