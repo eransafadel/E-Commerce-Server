274315d6f1df0666c53a7e55a733791768e2be81
= <eransafadel@gmail.com>
2022-08-03 02:01:09 +0300

    commit change

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const { register,login } = require("../controllers/auth");
 
-
 router.post("/register",register);
 router.post("/login",login);
 module.exports = router;
