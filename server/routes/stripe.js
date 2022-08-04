cec56e0cbd4043a2d13bdb028fe4e83854739a4e
= <eransafadel@gmail.com>
2022-08-04 10:58:20 +0300

    stripe api payment

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -2,21 +2,16 @@ const router = require("express").Router();
 const KEY = process.env.STRIPE_KEY
 const stripe = require("stripe")(KEY);
 
-router.post("/payment", (req, res) => {
-  stripe.charges.create(
+router.post("/payment", async (req, res) => {  
+  const charge = await stripe.charges.create(
     {
       source: req.body.tokenId,
       amount: req.body.amount,
       currency: "usd",
-    },
-    (stripeErr, stripeRes) => {
-      if (stripeErr) {
-        res.status(500).json(stripeErr);
-      } else {
-        res.status(200).json(stripeRes);
-      }
-    }
-  );
-});
+    });
+  res.status(200).json(charge);
+}
+
+);
 
 module.exports = router
